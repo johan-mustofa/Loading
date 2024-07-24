9579d5dbc6efce6c856ccb929bc5bf3418cd537c
Johan Mustofa <90010386+johan-mustofa@users.noreply.github.com>
2024-07-24 18:59:21 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Simulate page load
+window.addEventListener('load', function() {
+    const loaderWrapper = document.querySelector('.loader-wrapper');
+    loaderWrapper.classList.add('loaded');
+});
